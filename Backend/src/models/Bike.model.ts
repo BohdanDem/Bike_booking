7009a87a9c85c3669e90635101e7d93f8229f756
BohdanDem <bogdandemchuk.1@gmail.com>
2023-12-28 00:10:22 +0200

    connect to DB

--- Backend/src/models/Bike.model.ts
+++ Backend/src/models/Bike.model.ts
@@ -2,6 +2,10 @@ import { model, Schema } from "mongoose";
 
 import { IBike } from "../types/bike.type";
 
-const bikeSchema = new Schema({});
+const bikeSchema = new Schema({
+  price: {
+    type: Number,
+  },
+});
 
 export const Bike = model<IBike>("bike", bikeSchema);
