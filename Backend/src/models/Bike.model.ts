51ae1002e5b152b855fa2e9fc4022c63a387dc89
BohdanDem <bogdandemchuk.1@gmail.com>
2023-12-28 00:02:00 +0200

    connect to DB

--- Backend/src/models/Bike.model.ts
+++ Backend/src/models/Bike.model.ts
@@ -1,10 +1,1 @@
-import { model, Schema } from "mongoose";
-import {IBike} from "../types/bike.type";
-
-const bikeSchema = new Schema({
-  price: {
-    type: Number,
-  },
-});
-
-export const Bike = model<IBike>("bike", bikeSchema);
+import { model, Schema} from "mongoose";
